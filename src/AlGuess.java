a28525227f0a7345e5a792c8f5ea2a46f5a76f2a
BT_1N3_16 <gliu3637@bths.edu>
2018-09-25 10:14:56 -0400

    new changes

---
+++ src/AlGuess.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class AlGuess {
+    public static void main(String[] args) {
+        System.out.println("Please enter a number between 1 and 100(inclusive)");
+
+        int AlNum=50;
+        System.out.print(AlNum);
+        System.out.println("Is this higher or lower compare to your number? Or it is correct? Please tell me.");
+
+    }
+}
--- src/guessTheNumber.java
+++ src/guessTheNumber.java
@@ -2,23 +2,28 @@ import java.util.Scanner;
 public class guessTheNumber {
  public static void main(String[] args)
     {
-        int random=(int)(Math.random()*10+1);
-        System.out.println("Enter a number between 1 to 10");
-        Scanner input=new Scanner(System.in);
-        int guess;
-        guess = input.nextInt();
 
-        if(guess != random)
-        {
-            System.out.println("Keep guessing");
+         {
+            int random = (int) (Math.random() * 10 + 1);
+            System.out.println("Enter a number between 1 to 10");
+            Scanner input = new Scanner(System.in);
+            int guess;
             guess = input.nextInt();
-           
-        }
-        else
-        {
-            System.out.println("You got the right number.");
 
 
-        }
+          while(guess !=random)
+          {
+              System.out.println("Keep guessing");
+              Scanner input1=new Scanner(System.in);
+              guess = input1.nextInt();
+          }
+
+             System.out.println("You got the right number!1The number is "+ random);
+
+
+
+           }
+          
     }
 }
+
